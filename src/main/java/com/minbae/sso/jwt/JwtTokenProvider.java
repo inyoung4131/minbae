77be63c4378b8dc8d265c6000d88b1c678fca9f6
inyoung4131 <shoush@naver.com>
2022-04-29 15:21:44 +0900

    feat:jwt-v1

--- src/main/java/com/minbae/sso/jwt/JwtTokenProvider.java
+++ src/main/java/com/minbae/sso/jwt/JwtTokenProvider.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,25 +31,25 @@ public class JwtTokenProvider {
     private final long accessExpireTime = 172800000; //(60 * 60 * 1000L) * 48
     private final long refreshExpireTime = 172800000; // (60 * 60 * 1000L) * 48
 
-    public String createToken(String memberId, long memberIdx) {
+    public String createToken(String memberId, long memberIdx,String role) {
         Date now = new Date();
         Date validity = new Date(now.getTime() + accessExpireTime);
         return Jwts.builder()
                 .setSubject(""+memberId)
-                .setClaims(createClaims(memberId, tokenIss, memberIdx))
+                .setClaims(createClaims(memberId, tokenIss, memberIdx,role))
                 .setIssuedAt(now)
                 .setExpiration(validity)
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
-    public String createRefreshToken(String memberId,  int memberIdx) {
+    public String createRefreshToken(String memberId,  int memberIdx,String role) {
         Date now = new Date();
         Date validity = new Date(now.getTime() + refreshExpireTime);
 
         return Jwts.builder()
                 .setSubject(""+memberId)
-                .setClaims(createClaims(memberId, tokenIss, memberIdx))
+                .setClaims(createClaims(memberId, tokenIss, memberIdx,role))
                 .setIssuedAt(now)
                 .setExpiration(validity)
                 .signWith(SignatureAlgorithm.HS256, secretKey)
@@ -63,13 +64,25 @@ public class JwtTokenProvider {
             return false;
         }
     }
+    public boolean validateExpTokenForRole(String jwtToken,String role) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            System.out.println("토큰 롤 값"+claims.getBody().get("Role"));
+            if(claims.getBody().get("Role").equals(role))return true;
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-    private Map<String, Object> createClaims(String memberId, String tokenIss, long memberIdx) {
+    private Map<String, Object> createClaims(String memberId, String tokenIss, long memberIdx,String role) {
         Map<String, Object> claims = new HashMap<>();
 
         claims.put(claimUid, memberId);
         claims.put(claimIss, tokenIss);
         claims.put(claimMemberIdx, memberIdx);
+        claims.put("Role",role);
         
         return claims;
     }
--- src/main/java/com/minbae/test.java
+++ src/main/java/com/minbae/test.java
@@ -10,4 +10,5 @@ public class test {
     public String test(){
         return "test";
     }
+
 }
