e3643512d82a228237b11db20e3be4c6f7ce1fa4
inyoung4131 <shoush@naver.com>
2022-04-29 10:57:08 +0900

    test:jwt

--- src/main/java/com/minbae/deliver/repository/DeliverRepository.java
+++ src/main/java/com/minbae/deliver/repository/DeliverRepository.java
@@ -1,7 +1,12 @@
 package com.minbae.deliver.repository;
 
 import com.minbae.deliver.entity.Deliver;
+import com.minbae.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DeliverRepository extends JpaRepository<Deliver,Long> {
+
+    Optional<Deliver> findByDeliverEmailAndDeliverPwd(String email, String pwd);
 }
