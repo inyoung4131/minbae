9edeb371bc567e33e03d8f00c96a0f1f6e20db87
DevHyundo <08hyundo@gmail.com>
2022-05-03 17:51:46 +0900

    feat: menu update, image upload

--- src/main/java/com/minbae/menu/controller/MenuController.java
+++ src/main/java/com/minbae/menu/controller/MenuController.java
@@ -4,14 +4,18 @@ import com.minbae.menu.entity.Menu;
 import com.minbae.menu.service.MenuService;
 import com.minbae.store.entity.Store;
 import com.minbae.store.service.StoreService;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
+import java.util.*;
+import java.io.*;
 
 @RequiredArgsConstructor
 @Controller
@@ -37,4 +41,15 @@ public class MenuController {
         model.addAttribute("storeIdx",storeIdx);
         return "/menu/menu_create_form";
     }
+
+    // 이미지 출력
+    @GetMapping(value = "/image/{imagename}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> userSearch(@PathVariable("imagename") String imagename) throws IOException {
+        InputStream imageStream = new FileInputStream("C:\\workspace\\upload\\" + imagename);
+        byte[] imageByteArray = IOUtils.toByteArray(imageStream);
+        imageStream.close();
+        return new ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK);
+    }
+
+
 }
