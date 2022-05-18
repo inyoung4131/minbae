82e23a8983f2169e097af80d63518ea905aee4f9
DevHyundo <08hyundo@gmail.com>
2022-05-18 17:48:39 +0900

    feat: FlagController, fix: flagcreate.mustache

--- src/main/java/com/minbae/comm/flag/controller/FlagController.java
+++ src/main/java/com/minbae/comm/flag/controller/FlagController.java
@@ -1,22 +1,16 @@
 package com.minbae.comm.flag.controller;
 
-import com.minbae.comm.flag.dto.FlagSaveDto;
-import com.minbae.comm.flag.serivce.FlagService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RequiredArgsConstructor
-@RestController
+
+@Controller
 public class FlagController {
 
-    private final FlagService flagService;
-
-    @PostMapping("/owner/create/flag")
-    public String createFlag(@RequestBody FlagSaveDto flagSaveDto){
-        System.out.println(flagSaveDto.getFlagLat());
-        return flagService.createFlag(flagSaveDto)>0?"등록이 완료되었습니다":"등록 실패했습니다";
+    @GetMapping("/owner/flagpage/{storeIdx}")
+    public String goFlagPage(@PathVariable String storeIdx){
+        return "flag/flagcreate";
     }
 
 }
